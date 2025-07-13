443661b4d7c182eb07228c423594dac40ce5fb24
Natasha Wambui Hinga <natashahinga58@gmail.com>
2025-07-13 23:37:19 +0300

    code drop

--- jpwebsite/settings.py
+++ jpwebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#*%p#97g^8bashb@4#%@brs0myl3!dd%jrs6yqo%l5z(n*mpva
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(',')
 
 
 # Application definition
