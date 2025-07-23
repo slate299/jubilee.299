a2f3f5033a87941ddd5b1be1f90d0d0fec393ed3
Natasha Wambui Hinga <natashahinga58@gmail.com>
2025-07-23 23:44:46 +0300

    updates

--- jpwebsite/settings.py
+++ jpwebsite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,9 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+import os
 
-STATICFILES_DIRS = [ BASE_DIR / "jpwebsiteapp/static" ]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional extra static dirs for local dev
+# STATICFILES_DIRS = [ BASE_DIR / "jpwebsiteapp/static" ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
